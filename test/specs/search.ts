710717b9ad2c732bb14e4483c57c21104f43cb0c
Ilya Tyupo <derherrmannelig@gmail.com>
2024-01-26 20:40:02 +0200

    fix: make pauses in 'search' case longer for stability

--- test/specs/search.ts
+++ test/specs/search.ts
@@ -15,16 +15,16 @@ describe('Search input', () => {
     await expect(await mainPage.searchHistory).toBeDisplayed();
     await expect(await mainPage.searchServicesCategories).toBeDisplayed();
     await browser.keys('\uE007');
-    await browser.pause(1000);
+    await browser.pause(1500);
     await expect(browser).toHaveUrl(`${process.env.ENV}products/`);
     await expect(await mainPage.searchInput).toHaveValue('');
     await expect(await mainPage.catalogUnits).toBeDisplayed();
     await mainPage.clickNavLogo();
-    await browser.pause(1000);
+    await browser.pause(1500);
     await mainPage.clickSearchInput();
     await mainPage.searchInput.setValue(`${testData.searchInput1}`);
     await browser.keys('\uE007');
-    await browser.pause(1000);
+    await browser.pause(1500);
     await expect(browser).toHaveUrl(`${process.env.ENV}products/`);
     await expect(await mainPage.searchMap).toBeDisplayed();
     await expect(await mainPage.searchResult).toBeDisplayed();
@@ -32,14 +32,14 @@ describe('Search input', () => {
     await mainPage.clickFirstCatalogUnit();
     await expect(browser).toHaveUrl(expect.stringContaining(`unit`));
     await mainPage.clickNavLogo();
-    await browser.pause(1000);
+    await browser.pause(1500);
     await mainPage.clickSearchInput();
     await expect(await mainPage.searchHistory).toBeDisplayed();
     await expect(await mainPage.searchServicesCategories).toBeDisplayed();
     await expect(await mainPage.searchHistoryFirst).toHaveText(`${testData.searchInput1}`);
     await mainPage.searchInput.setValue(`${testData.searchInput2}`);
     await browser.keys('\uE007');
-    await browser.pause(1000);
+    await browser.pause(1500);
     await expect(browser).toHaveUrl(`${process.env.ENV}products/`);
     await expect(await mainPage.searchMap).toBeDisplayed();
     await expect(await mainPage.searchResult).toBeDisplayed();
@@ -47,52 +47,52 @@ describe('Search input', () => {
     await mainPage.clickFirstCatalogUnit();
     await expect(browser).toHaveUrl(expect.stringContaining(`unit`));
     await mainPage.clickNavLogo();
-    await browser.pause(1000);
+    await browser.pause(1500);
     await mainPage.clickSearchInput();
     await expect(await mainPage.searchHistory).toBeDisplayed();
     await expect(await mainPage.searchServicesCategories).toBeDisplayed();
     await expect(await mainPage.searchHistoryFirst).toHaveText(`${testData.searchInput2}`);
     await mainPage.searchInput.setValue(`${testData.searchInput3}`);
-    await browser.pause(1000);
+    await browser.pause(1500);
     await mainPage.clickFirstSearchUnit();
     await expect(browser).toHaveUrl(expect.stringContaining(`unit`));
     await expect(await mainPage.unitTitle).toHaveText(expect.stringContaining(`${testData.searchInput3}`));
     await mainPage.clickNavLogo();
     // for some reason, steps where search units are checked need small pause to ensure stability
-    await browser.pause(1000);
+    await browser.pause(1500);
     await mainPage.clickSearchInput();
     await mainPage.searchInput.setValue(`${testData.searchInput4}`);
-    await browser.pause(1000);
+    await browser.pause(1500);
     await expect(await mainPage.searchUnits).not.toBeExisting();
     await browser.keys('\uE007');
-    await browser.pause(1000);
+    await browser.pause(1500);
     await expect(browser).toHaveUrl(`${process.env.ENV}products/`);
     await expect(await mainPage.searchResult).toHaveText(`Знайдено 0 оголошень на видимій території за запитом " "`);
     await mainPage.clickNavLogo();
-    await browser.pause(1000);
+    await browser.pause(1500);
     await mainPage.clickSearchInput();
     await mainPage.searchInput.setValue(`${testData.searchInput5}`);
-    await browser.pause(1000);
+    await browser.pause(1500);
     await expect(await mainPage.searchUnits).toBeExisting();
     await browser.keys('\uE007');
-    await browser.pause(1000);
+    await browser.pause(1500);
     await expect(browser).toHaveUrl(`${process.env.ENV}products/`);
     await expect(await mainPage.searchResult).toBeDisplayed();
     await expect(await mainPage.searchResult).toHaveText(expect.stringContaining(`${testData.searchInput5}`));
     await mainPage.clickFirstCatalogUnit();
     await expect(browser).toHaveUrl(expect.stringContaining(`unit`));
     await mainPage.clickNavLogo();
-    await browser.pause(1000);
+    await browser.pause(1500);
     await mainPage.clickSearchInput();
     await mainPage.searchInput.setValue(`${randomSymbol}`);
-    await browser.pause(1000);
+    await browser.pause(1500);
     if (await mainPage.searchUnits[0].isExisting()) {
       await expect(await mainPage.searchUnits).toBeDisplayed();
     } else {
       await expect(await mainPage.searchUnits).not.toBeDisplayed();
     }
     await browser.keys('\uE007');
-    await browser.pause(1000);
+    await browser.pause(1500);
     await expect(browser).toHaveUrl(`${process.env.ENV}products/`);
     if (!['<', '>', '^', ';', '{', '}','`'].includes(randomSymbol)) {
       await expect(await mainPage.searchInput).toHaveValue(`${randomSymbol}`);
@@ -102,20 +102,20 @@ describe('Search input', () => {
       await expect(await mainPage.catalogUnits).toBeDisplayed();
     }
     await mainPage.clickNavLogo();
-    await browser.pause(1000);
+    await browser.pause(1500);
     await mainPage.clickSearchInput();
     await mainPage.searchInput.setValue(`${testData.searchInput6}`);
-    await browser.pause(1000);
+    await browser.pause(1500);
     await expect(await mainPage.searchUnits).not.toBeExisting();
     await browser.keys('\uE007');
-    await browser.pause(1000);
+    await browser.pause(1500);
     await expect(browser).toHaveUrl(`${process.env.ENV}products/`);
     await expect(await mainPage.searchResult).toHaveText(`Знайдено 0 оголошень на видимій території за запитом "${testData.searchInput6}"`);
     await mainPage.clickNavLogo();
-    await browser.pause(1000);
+    await browser.pause(1500);
     await mainPage.clickSearchInput();
     await mainPage.searchInput.setValue(`${testData.searchInput7}`);
-    await browser.pause(1000);
+    await browser.pause(1500);
     await expect(await mainPage.searchUnits).toBeExisting();
     await expect(await mainPage.searchCertainService(`${testData.searchInput7}`)).toBeExisting();
     await (await mainPage.searchCertainService(`${testData.searchInput7}`)).click();
@@ -124,10 +124,10 @@ describe('Search input', () => {
     await expect(await mainPage.searchResult).toBeDisplayed();
     await expect(await mainPage.searchResult).toHaveText(expect.stringContaining(`Знайдено`));
     await mainPage.clickNavLogo();
-    await browser.pause(1000);
+    await browser.pause(1500);
     await mainPage.clickSearchInput();
     await mainPage.searchInput.setValue(`${testData.searchInput8}`);
-    await browser.pause(1000);
+    await browser.pause(1500);
     await expect(await mainPage.searchUnits).toBeExisting();
     await expect(await mainPage.searchCertainCategory(`${testData.searchInput8.toLowerCase()}`)).toBeExisting();
     await (await mainPage.searchCertainCategory(`${testData.searchInput8.toLowerCase()}`)).click();
@@ -136,12 +136,12 @@ describe('Search input', () => {
     await expect(await mainPage.searchResult).toBeDisplayed();
     await expect(await mainPage.searchResult).toHaveText(expect.stringContaining(`Знайдено`));
     await mainPage.clickNavLogo();
-    await browser.pause(1000);
+    await browser.pause(1500);
     await mainPage.clickSearchInput();
     await mainPage.searchInput.setValue(`${testData.searchInput3}`);
     await expect(await mainPage.searchHistory).toBeDisplayed();
     await expect(await mainPage.searchServicesCategories).toBeDisplayed();
-    await browser.pause(1000);
+    await browser.pause(1500);
     await expect(await mainPage.searchUnits).toBeDisplayed();
     await mainPage.clickSearchClear();
     await expect(await mainPage.searchInput).toHaveValue('');
@@ -150,7 +150,7 @@ describe('Search input', () => {
     // that step wasn't described fully in list
     // implemented first part of it as a bonus to ensure if history feature works properly
     await browser.refresh();
-    await browser.pause(1000);
+    await browser.pause(1500);
     await mainPage.clickSearchInput();
     await expect(await mainPage.searchHistoryFirst).toHaveText(`${testData.searchInput3}`);
   })
